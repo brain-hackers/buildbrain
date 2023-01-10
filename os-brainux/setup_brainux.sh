9f41a0bbd202dbff924f02d5eaf0f31cfe2598f6
Takumi Sueda <puhitaku@gmail.com>
2023-01-10 19:14:43 +0900

    Switch ly repository to freeze the source code

--- os-brainux/setup_brainux.sh
+++ os-brainux/setup_brainux.sh
@@ -101,7 +101,7 @@ systemctl enable fake-hwclock
 DEBIAN_FRONTEND=noninteractive \
     apt install -y libpam0g-dev libxcb-xkb-dev
 cd /
-git clone --recurse-submodules https://github.com/nullgemm/ly.git
+git clone --recurse-submodules -b master-24f017e https://github.com/brain-hackers/ly.git
 cd ly
 make
 make install
