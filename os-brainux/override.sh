35f04a7b11019786687e5168d84469a539ecee59
Takumi Sueda <puhitaku@gmail.com>
2023-02-26 00:45:45 +0900

    Override logind.conf to enable suspend with the power key

--- os-brainux/override.sh
+++ os-brainux/override.sh
@@ -11,6 +11,8 @@ install -g root -o root -m 0440 $SRC/etc/sudoers $DST/etc/sudoers
 install -g root -o root -m 0644 $SRC/etc/X11/xorg.conf $DST/etc/X11/xorg.conf
 install -g root -o root -m 0644 $SRC/etc/X11/Xsession.d/96calibrate $DST/etc/X11/Xsession.d/96calibrate
 
+install -g root -o root -m 0644 $SRC/etc/systemd/logind.conf $DST/etc/systemd/logind.conf
+
 install -g root -o root -m 0644 -D $SRC/etc/xdg/weston/weston.ini $DST/etc/xdg/weston/weston.ini
 
 install -g 1000 -o 1000 -m 0644 $SRC/home/user/.xprofile $DST/home/user/.xprofile
